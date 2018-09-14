1e480b53a936f09ff1d5850062734043eeb65c40
Danny <danny.v.dam@gmail.com>
2018-09-14 22:00:29 +0200

    Match real data and write as CSV to stdout

--- src/main/scala/nl/dvandam/meterkast/Meterkast.scala
+++ src/main/scala/nl/dvandam/meterkast/Meterkast.scala
@@ -13,26 +13,26 @@ object Meterkast extends App {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-  val valuePattern = """((?:hoog|laag)[pu]): (\d+)""".r
+  val valuePattern = """1-0:([12].8.[12]).(\d+).(\d+).kWh.*""".r
 
   val done: Future[Done] = Source.fromIterator { () => StdInReader }
     .scan(Builder(Map.empty, None)) {
-      case (_, "start") => Builder(Map.empty, None)
-      case (Builder(data, _), "end") => Builder(Map.empty, for {
-        usageLow <- data.get("laagu")
-        usageHigh <- data.get("hoogu")
-        productionLow <- data.get("laagp")
-        productionHigh <- data.get("hoogp")
+      case (_, "/ISk5\\2MT382-1003") => Builder(Map.empty, None) // start
+      case (Builder(data, _), "!") => Builder(Map.empty, for { // end
+        usageLow <- data.get("1.8.1")
+        usageHigh <- data.get("1.8.2")
+        productionLow <- data.get("2.8.1")
+        productionHigh <- data.get("2.8.2")
       } yield {
         Record(Usage(usageLow, usageHigh), Production(productionLow, productionHigh))
       })
-      case (Builder(data, _), valuePattern(field, value)) => Builder(data + (field -> value.toInt), None)
-      case (builder, _) => builder.copy(record = None)
+      case (Builder(data, _), valuePattern(field, kilo, watt)) => Builder(data + (field -> (kilo + watt).toInt), None)
+      case (builder, line) => builder.copy(record = None)
     }
     .collect {
       case Builder(_, Some(record)) => record
     }
-    .runForeach(i => println(i))(materializer)
+    .runForeach(r => println(s"${r.usage.low},${r.usage.high},${r.production.low},${r.production.high}"))(materializer)
 
   done.onComplete(_ => system.terminate())
 }
